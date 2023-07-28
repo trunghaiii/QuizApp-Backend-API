210f507f10fd47b40a1b69d53e6f3105bdf2444d
trunghaiii <haitran022001@gmail.com>
2023-07-27 23:30:04 -0400

    change knex config 2

--- src/config/knexConfig.js
+++ src/config/knexConfig.js
@@ -2,11 +2,15 @@ require('dotenv').config()
 const postgresDb = require('knex')({
     client: 'pg',
     connection: {
-        host: 'postgresql-octagonal-79620' || process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: 'postgres'
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+        // host: 'postgresql-octagonal-79620' || process.env.DB_HOST,
+        // port: Number(process.env.DB_PORT),
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASS,
+        // database: 'postgres'
     }
 });
 
